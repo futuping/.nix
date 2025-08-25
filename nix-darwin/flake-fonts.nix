6d7cb79cbbe8b5cc2fad383d5d8e0fea84862003
Admin <admin@MacBook-Air.local>
2025-08-25 04:29:58 -0700

    fix(fonts): improve font installation robustness
    - Use find command to selectively copy font files by extension
    - Prevents copying non-font files and directories
    - Supports .otf, .ttf, .woff, and .woff2 formats
    
    修复(字体): 提高字体安装的健壮性
    
    - 使用 find 命令按扩展名选择性复制字体文件
    - 防止复制非字体文件和目录
    - 支持 .otf、.ttf、.woff 和 .woff2 格式

--- nix-darwin/flake-fonts.nix
+++ nix-darwin/flake-fonts.nix
@@ -7,7 +7,9 @@
       src = ./fonts;
       installPhase = ''
         mkdir -p $out/share/fonts
-        cp -R * $out/share/fonts/
+        find . -name "*.otf" -o -name "*.ttf" -o -name "*.woff" -o -name "*.woff2" | while read font; do
+          cp "$font" $out/share/fonts/
+        done
       '';
     })
   ];
