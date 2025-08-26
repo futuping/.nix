08603c1a88177ebe9b0c3b3c7ea1a9d7821f4fc9
Admin <admin@MacBook-Air.local>
2025-08-26 02:09:25 -0700

    Fix local fonts installation script

--- nix-darwin/flake-fonts.nix
+++ nix-darwin/flake-fonts.nix
@@ -1,14 +1,34 @@
 { pkgs, ... }:
 
 {
-  fonts.packages = [
-    (pkgs.stdenv.mkDerivation {
-      name = "local-fonts";
-      src = ./fonts;
-      installPhase = ''
-        mkdir -p $out/share/fonts
-        cp -R * $out/share/fonts/
-      '';
-    })
-  ];
+  fonts = {
+    packages = with pkgs; [
+      # System fonts from nixpkgs
+      nerd-fonts.jetbrains-mono
+
+      # Local fonts from the fonts/ directory
+      (pkgs.stdenv.mkDerivation {
+        name = "local-fonts";
+        src = ./fonts;
+        installPhase = ''
+          mkdir -p $out/share/fonts/opentype
+          mkdir -p $out/share/fonts/truetype
+
+          # Copy OTF fonts
+          for otf in *.otf; do
+            if [ -f "$otf" ]; then
+              cp "$otf" $out/share/fonts/opentype/
+            fi
+          done
+
+          # Copy TTF fonts
+          for ttf in *.ttf; do
+            if [ -f "$ttf" ]; then
+              cp "$ttf" $out/share/fonts/truetype/
+            fi
+          done
+        '';
+      })
+    ];
+  };
 }
