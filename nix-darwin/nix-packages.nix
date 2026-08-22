860e3bfa4e77785376ab4c1ca83bc00db4818a31
Tuping Fu <45912467+futuping@users.noreply.github.com>
2026-08-21 23:39:44 -1000

    feat(darwin): add ShardX Launcher

--- nix-darwin/nix-packages.nix
+++ nix-darwin/nix-packages.nix
@@ -8,10 +8,12 @@
   imports = [
     inputs.nix-packages.darwinModules.ego-lite
     inputs.nix-packages.darwinModules.lite-xl-app
+    inputs.nix-packages.darwinModules.shardx-launcher
   ];
 
   environment.systemPackages = with pkgs; [
     ego-lite
     lite-xl-app
+    shardx-launcher
   ];
 }
