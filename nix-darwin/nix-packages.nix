c156b055e143d95019da1168b2163e1654742893
Tuping Fu <45912467+futuping@users.noreply.github.com>
2026-08-28 06:59:38 -1000

    refactor(darwin): select Neomacs by bare name

--- nix-darwin/nix-packages.nix
+++ nix-darwin/nix-packages.nix
@@ -8,16 +8,14 @@
   imports = [
     inputs.nix-packages.darwinModules.ego-lite
     inputs.nix-packages.darwinModules.lite-xl-app
+    inputs.nix-packages.darwinModules.neomacs
     inputs.nix-packages.darwinModules.shardx-launcher
   ];
 
-  environment.systemPackages =
-    (with pkgs; [
-      ego-lite
-      lite-xl-app
-      shardx-launcher
-    ])
-    ++ [
-      inputs.nix-packages.packages.${pkgs.stdenv.hostPlatform.system}.neomacs
-    ];
+  environment.systemPackages = with pkgs; [
+    ego-lite
+    lite-xl-app
+    neomacs
+    shardx-launcher
+  ];
 }
